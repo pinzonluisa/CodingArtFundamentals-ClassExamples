ebb0de5e2923ccc7d6640e1a812b3da26f96555b
Luisa Pinzón <98370036+pinzonluisa@users.noreply.github.com>
2026-09-23 18:18:11 -0700

    changevarialbes

--- Week3/4-LoopsIntro-noFill.js
+++ Week3/4-LoopsIntro-noFill.js
@@ -1,25 +1,27 @@
+let x,y,scale;
 function setup() {
   // Create a canvas that fills the entire browser window
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
 
-  x = width/2; 
-  y = height/2; 
-  //scale = 40;
+  x = width/2;
+  y = height/2;
+  scale = 40;
 
 }
 
 function draw() {
 background(0);
- 
-for(i=0; i <10 ; i++){
 
-  //noFill(); 
+
+for(i = 0; i < 20 ; i++)
+{
+  noFill();
   stroke('blue');
-  strokeWeight(3);
-  rect(x,y,i*20,i*20);
+  strokeWeight(4);
+  rect(x,y,i*scale,i*scale);
+}
 
 }
 
 
-}

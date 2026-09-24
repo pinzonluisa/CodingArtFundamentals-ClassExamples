e2bce3f4ddfc90c54d84e10b9d8baf3ed9f10673
Luisa Pinzón <98370036+pinzonluisa@users.noreply.github.com>
2026-09-23 17:57:50 -0700

    correct rectangle class
    Fix typo in variable name and improve formatting.

--- Week3/3-rectIntro.js
+++ Week3/3-rectIntro.js
@@ -1,18 +1,20 @@
+let x,y; 
+
 function setup() {
   // Create a canvas that fills the entire browser window
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
 
-  x = width/2; 
+  x = width/2;
   y = height/2; 
-  sclae = width;
 
+  
 }
 
 function draw() {
 background(0);
- 
-fill('orange'); 
+
+fill('orange');
 strokeWeight(3);
 rect(x,y,width/2,height);
 
